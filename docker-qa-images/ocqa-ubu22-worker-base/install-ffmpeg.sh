c467cc84eabd738f187db266cd9daf2cb97b3c63
Greg Logan <gregorydlogan@gmail.com>
2025-10-11 22:08:29 -0600

    Disabling the OC repo in centos, then reenabling it with the ffmpeg install script

--- docker-qa-images/ocqa-ubu22-worker-base/install-ffmpeg.sh
+++ docker-qa-images/ocqa-ubu22-worker-base/install-ffmpeg.sh
@@ -7,5 +7,7 @@ if command -v apt-get > /dev/null; then
   sudo -n apt-get install -y ffmpeg-dist
 elif command -v yum > /dev/null; then 
   sudo -n sed -i "s/FIXME/$branch/g" /etc/yum.repos.d/opencast.repo && \
+  #NB two spaces to keep things aligned, do not change unless repo file changes!
+  sudo -n sed -i 's/enabled  = 0/enabled  = 1/g' /etc/yum.repos.d/opencast.repo && \
   sudo -n yum install -y ffmpeg
 fi
