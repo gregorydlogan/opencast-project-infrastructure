43a609481f806434e634aab5dd55df3f67eebf56
Greg Logan <gregorydlogan@gmail.com>
2025-12-08 08:05:09 -0600

    Reducing number of build attempts for reports since they're so expensive timewise to do

--- ansible-buildbot-cluster/roles/buildbot-config/templates/reports.py
+++ ansible-buildbot-cluster/roles/buildbot-config/templates/reports.py
@@ -67,8 +67,7 @@ class Reports():
             '-P', 'none,!frontend'
         ]
         site = common.getBuild(override=reports, name="Build site report", haltOnFailure=False)
-        site2 = common.getBuild(override=reports, name="Build site report attempt 2", haltOnFailure=False, doStepIf=lambda step: step.build.results != SUCCESS)
-        site3 = common.getBuild(override=reports, name="Build site report attempt 3", doStepIf=lambda step: step.build.results != SUCCESS)
+        site2 = common.getBuild(override=reports, name="Build site report attempt 2", doStepIf=lambda step: step.build.results != SUCCESS)
 
         f_build = util.BuildFactory()
         f_build.addStep(common.getPreflightChecks())
@@ -83,7 +82,6 @@ class Reports():
         f_build.addStep(checkSpaces)
         f_build.addStep(site)
         f_build.addStep(site2)
-        f_build.addStep(site3)
 
         return f_build
 
