370a47e67443235d4364e18f3dd18f5915974c1c
Greg Logan <gregorydlogan@gmail.com>
2026-06-04 09:47:20 -0600

    Addressing shellcheck complaints

--- docker-qa-images/pull.sh
+++ docker-qa-images/pull.sh
@@ -2,7 +2,6 @@
 
 DOCKER_OWNER=greglogan
 DOCKER_TAG=latest
-BUILDBOT_VERSION="v4.3.0"
 
 if [ $# -gt 1 ]; then
   echo "Usage: $0 [TAG]"
@@ -13,16 +12,16 @@ fi
 
 ls | grep worker-base | cut -f 2 -d "-" | while read image
 do
-  pushd . 2>&1 > /dev/null
-  cd ocqa-$image-worker-base
-  docker pull $DOCKER_OWNER/ocqa-$image-worker-base-core:$DOCKER_TAG
-  docker pull $DOCKER_OWNER/ocqa-$image-worker-base:$DOCKER_TAG
-  grep "FROM .* AS .*" Dockerfile | cut -f 4 -d " " | grep jdk | while read subimage
+  pushd . > /dev/null 2>&1
+  cd "ocqa-$image-worker-base" || exit
+  docker pull "$DOCKER_OWNER/ocqa-$image-worker-base-core:$DOCKER_TAG"
+  docker pull "$DOCKER_OWNER/ocqa-$image-worker-base:$DOCKER_TAG"
+  grep "FROM .* AS .*" Dockerfile | cut -f 4 -d " " | grep jdk | while read -r subimage
   do
-    docker pull $DOCKER_OWNER/ocqa-$image-worker-base-$subimage:$DOCKER_TAG
+    docker pull "$DOCKER_OWNER/ocqa-$image-worker-base-$subimage:$DOCKER_TAG"
   done
-  popd 2>&1 > /dev/null
+  popd > /dev/null 2>&1 || exit
 done
-docker pull $DOCKER_OWNER/ocqa-buildbot-master:$DOCKER_TAG
+docker pull "$DOCKER_OWNER/ocqa-buildbot-master:$DOCKER_TAG"
 
 docker image prune -f
