ec908902f4355ce5dac643072d3abfdd01006ef9
Greg Logan <gregorydlogan@gmail.com>
2026-06-09 22:40:51 -0600

    Updated restore, since this should be semi polished

--- ansible-buildbot-cluster/roles/buildbot-config/templates/restore_database.sh
+++ ansible-buildbot-cluster/roles/buildbot-config/templates/restore_database.sh
@@ -5,4 +5,14 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-cat $1 | bunzip2 - | docker compose -f /opt/buildbot/docker-compose.yml exec -T db bash -c 'PGPASSWORD=change_me psql -h db -U buildbot buildbot'
+if [ $(curl -s 'https://{{ inventory_hostname }}/api/v2/builds?limit=10' | jq -r '.meta.total') -lt 1 ]; then
+  echo "Restoring"
+  docker compose down
+  rm -rf postgres-data
+  docker compose up -d db
+  sleep 30
+  cat $1 | bunzip2 - | docker compose -f /opt/buildbot/docker-compose.yml exec -T db bash -c 'PGPASSWORD=change_me psql -h db -U buildbot buildbot'
+  docker compose up -d
+else
+  echo "Refusing to restore to an already active buildbot system"
+fi
